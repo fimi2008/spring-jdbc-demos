a3cb51157003c808f01e9c049346eb7222656d37
wangxiang <574693894@qq.com>
2016-04-26 17:52:52 +0800

    spring jdbc 项目练习

---
+++ spring-01-jdbc/src/test/java/com/shine/spring/jdbc/datasource/DataSourceTest.java
@@ -0,0 +1,28 @@
+package com.shine.spring.jdbc.datasource;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import javax.sql.DataSource;
+
+/**
+ * <p>Description: Spring jdbc 测试类 </p>
+ * <p>Company: 上海天坊信息科技有限公司</p>
+ *
+ * @author wangxiang
+ * @version 1.0
+ * @time 16/4/26 下午5:23
+ */
+public class DataSourceTest {
+
+    @Test
+    public void testDataSource() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        /**
+         * 面向接口编程
+         */
+        DataSource dataSource = (DataSource) context.getBean("dataSource");
+        System.out.println(dataSource);
+    }
+}
